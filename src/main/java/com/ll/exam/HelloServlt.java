5b69ca8d78db3df835ab2a9aaefa010731df5295
Young998904 <like0998904@gmail.com>
2022-07-19 17:45:54 +0900

    Add Servlet Library as provided & Make HelloServlt & Running Tomcat

--- src/main/java/com/ll/exam/HelloServlt.java
+++ src/main/java/com/ll/exam/HelloServlt.java
@@ -1,4 +1,27 @@
 package com.ll.exam;
 
-public class HelloServlt {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlt extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 한글로 하고 싶을 때
+
+        // 들어오는 파라미터를 UTF-8로 해석
+        req.setCharacterEncoding("UTF-8");
+        // 서블릿이 HTML 파일을 만들 때 UTF-8 로 쓰기
+        resp.setCharacterEncoding("UTF-8");
+
+        // HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
+        resp.setContentType("text/html; charset=utf-8");
+
+        resp.getWriter().append("안녕하세요!");
+    }
 }
